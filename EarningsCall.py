108b875fd2a169227800bc52d61411bc004a53ef
Jonathan Yong <30435623+jonathanyong@users.noreply.github.com>
2019-12-19 17:00:15 +0800

    Add files via upload

---
+++ EarningsCall.py
@@ -0,0 +1,109 @@
+import re
+from pathlib import Path
+from random import random
+from time import sleep
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+from furl import furl
+from selenium import webdriver
+import pandas as pd
+
+transcript_path = Path('transcripts')
+
+# define function to parse through extracted earnings call from website
+def parse_html(html):
+    sleep(1)
+    date_pattern = re.compile(r'(\d{2})-(\d{2})-(\d{2})')
+    quarter_pattern = re.compile(r'(\bQ\d\b)')
+    soup = BeautifulSoup(html, 'lxml')
+    
+    meta, participants, content = {},[],[]
+    h1 = soup.find('h1', itemprop='headline')
+    if h1 is None:
+        return
+    h1 = h1.text
+    meta['company'] = h1[:h1.find('(')].strip()
+    meta['symbol'] = h1[h1.find('(') + 1:h1.find(')')]
+    
+    title = soup.find('div', class_='title')
+    if title is None:
+        return
+    title = title.text
+    print(title)
+    match = date_pattern.search(title)
+    if match:
+        m, d, y = match.groups()
+        meta['month'] = int(m)
+        meta['day'] = int(d)
+        meta['year'] = int(y)
+    
+    match = quarter_pattern.search(title)
+    if match:
+        meta['quarter'] = match.group(0)
+    
+    qa = 0
+    speaker_types = ['Executives','Analysts']
+    for header in [p.parent for p in soup.find_all('strong')]:
+        text = header.text.strip()
+        if text.lower().startswith('copyright'):
+            continue
+        elif text.lower().startswith('question-and'):
+            qa = 1
+            continue
+        elif any([type in text for type in speaker_types]):
+            for participant in header.find_next_sibilings('p'):
+                if participant.find('strong'):
+                    break
+                else:
+                    participants.append([text,participant.text])
+        else:
+            p = []
+            for participant in header.find_next_siblings('p'):
+                if participant.find('strong'):
+                    break
+                else:
+                    p.append(participant.text)
+            content.append([header.text, qa, '\n'.join(p)])
+    
+    return meta, participants, content
+
+# Define function to store parsed results into a csv file
+def store_result(meta, participants, content):
+    path = transcript_path / 'parsed' / meta['symbol']
+    if not path.exists():
+        path.mkdir(parents = True, exist_ok=True)
+    pd.DataFrame(content, columns = ['speaker','q&a','content']).to_csv(path / 'content.csv', index = False)
+    pd.DataFrame(participants, columns = ['type','name']).to_csv(path/'participants.csv',index = False)
+    pd.Series(meta).to_csv(path / 'earnings.csv')
+
+# Run the transcript and parse HTML
+SA_URL = 'http://seekingalpha.com/'
+TRANSCRIPT = re.compile('Earnings Call Transcript')
+
+next_page = True
+page = 1
+driver = webdriver.Firefox(executable_path = '/Users/jonat/downloads/geckodriver')
+while next_page:
+    sleep(1)
+    url = f'{SA_URL}/earnings/earnings-call-transcripts/{page}'
+    driver.get(urljoin(SA_URL, url))
+    response = driver.page_source
+    page += 1
+    soup = BeautifulSoup(response, 'lxml')
+    links = soup.find_all(name = 'a', string = TRANSCRIPT)
+    if len(links) == 0:
+        next_page = False
+    else:
+        for link in links:
+            transcript_url = link.attrs.get('href')
+            article_url = furl(urljoin(SA_URL, transcript_url)).add({'part': 'single'})
+            driver.get(article_url.url)
+            html = driver.page_source
+            result = parse_html(html)
+            if result is not None:
+                meta, participants, content = result
+                meta['link'] = link
+                store_result(meta, participants, content)
+            sleep(5 + (random() - .5) * 2)
+
+driver.close()
